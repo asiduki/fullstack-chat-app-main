23db9b7bf1e8177bf2264935b637f431cefa9f66
asiduki <147263246+asiduki@users.noreply.github.com>
2026-03-11 05:58:27 +0530

    Edit some settings

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.FRONTEND_URL}/api`,
+    baseURL: `${import.meta.env.VITE_FRONTEND_URL}/api`,
     withCredentials: true,
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BASE_URL = `${import.meta.env.FRONTEND_URL}/`
+const BASE_URL = `${import.meta.env.VITE_FRONTEND_URL}/`
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
