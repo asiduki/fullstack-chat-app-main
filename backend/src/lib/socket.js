0fff5da5eb7fb2e2f759fcb1cede7f043bef757b
asiduki <147263246+asiduki@users.noreply.github.com>
2026-03-11 06:35:59 +0530

    edit some cors

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,6 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
-console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173" , "https://fullstack-chat-app-main.vercel.app"],
